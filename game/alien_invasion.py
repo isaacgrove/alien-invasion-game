71442e33e2235262abaaec457cb45c7c9103f777
isaacgrove <isaacgrove333@gmail.com>
2021-01-12 11:39:39 -0500

    limits # of bullets

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -28,6 +28,11 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_screen(ai_settings, screen, ship, bullets)
+        
+        # Get rid of bullets once offscreen
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
             
 
 run_game()
